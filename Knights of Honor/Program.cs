809cd9c541b4d676cbe8272f54cabd312f763c07
Loran Abbas <loranabbas@abv.bg>
2022-06-02 18:37:08 +0300

    Add project files.

---
+++ Knights of Honor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, string> addPrefix = name => "Sir" + " " + name;
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(addPrefix(name));
+            }
+        }
+    }
+}
